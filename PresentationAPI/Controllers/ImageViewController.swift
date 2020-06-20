647115deea59f0a83cde323a971814eaaf33c6f3
Egor Ukolov <ukolovegor@yandex.ru>
2020-06-20 17:20:40 +0700

    Реализация сетевого запроса (download image)

---
+++ PresentationAPI/Controllers/ImageViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ImageViewController.swift
+//  PresentationAPI
+//
+//  Created by Egor Ukolov on 20.06.2020.
+//  Copyright © 2020 Egor Ukolov. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        activityIndicator.startAnimating()
+        activityIndicator.hidesWhenStopped = true
+        fetchImage()
+       
+    }
+    
+    private func fetchImage() {
+        
+        let stringURL = "https://i.pinimg.com/474x/2d/db/b5/2ddbb5f5a882e50ceba3d229c1a5dd5a.jpg"
+        guard let imageURL = URL(string: stringURL) else { return }
+        
+        URLSession.shared.dataTask(with: imageURL) { (data, response, error) in
+            if let error = error { print(error); return }
+            if let response = response { print(response) }
+            
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.activityIndicator.stopAnimating()
+                    self.imageView.image = image
+                }
+            }
+        }.resume()
+    }
+}
+
+
+
