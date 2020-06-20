8d3c018a7773a0b4c70de1bd1a529d4514245983
Egor Ukolov <ukolovegor@yandex.ru>
2020-06-20 12:11:16 +0700

    Создание интерфейса приложения; реализация CollectionView на главном экране; переход на последующие экраны по тапу ячейки коллекции

---
+++ PresentationAPI/Controllers/ApisCollectionViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ApisCollectionViewController.swift
+//  PresentationAPI
+//
+//  Created by Egor Ukolov on 20.06.2020.
+//  Copyright © 2020 Egor Ukolov. All rights reserved.
+//
+
+import UIKit
+
+enum UserActions: String, CaseIterable {
+    
+    case downloadImage = "Download Image"
+    case exampleOne = "Api Example One"
+    case exampleTwo = "Api Example Two"
+}
+
+class ApisCollectionViewController: UICollectionViewController {
+
+    let userActions = UserActions.allCases
+    
+    // MARK: UICollectionViewDataSource
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+       
+        userActions.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! UserActionCell
+        let userAction = userActions[indexPath.item]
+        cell.userActionLabel.text = userAction.rawValue
+        
+        return cell 
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let userAction = userActions[indexPath.item]
+        
+        switch userAction {
+        case .downloadImage:
+            performSegue(withIdentifier: "ShowImage", sender: nil)
+        case .exampleOne:
+             performSegue(withIdentifier: "ExampleOne", sender: nil)
+        case .exampleTwo:
+             performSegue(withIdentifier: "ExampleTwo", sender: nil)
+        }
+    }
+
+}
+
+extension ApisCollectionViewController: UICollectionViewDelegateFlowLayout {
+     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: UIScreen.main.bounds.width - 48, height: 100)
+    }
+    
+    
+}
---
+++ PresentationAPI/Controllers/UserActionCell.swift
@@ -0,0 +1,16 @@
+//
+//  UserActionCell.swift
+//  PresentationAPI
+//
+//  Created by Egor Ukolov on 20.06.2020.
+//  Copyright © 2020 Egor Ukolov. All rights reserved.
+//
+
+import UIKit
+
+class UserActionCell: UICollectionViewCell {
+    
+    @IBOutlet var userActionLabel: UILabel!
+    
+    
+}
